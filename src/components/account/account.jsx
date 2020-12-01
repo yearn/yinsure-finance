4f15672dadbb628575e9f0962ca4863464b2c0c9
Petro Kuzmychev <johnnyfearmekramer@gmail.com>
2020-12-01 17:43:15 +0400

    fix intro footer

--- src/components/account/account.jsx
+++ src/components/account/account.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { withStyles } from '@material-ui/core/styles';
-import {
-  Typography,
-  Button,
-} from '@material-ui/core';
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { withStyles } from '@material-ui/core/styles'
+import { Typography, Button } from '@material-ui/core'
 import { colors } from '../../theme'
 
 import UnlockModal from '../unlock/unlockModal.jsx'
 import ArmorIcon from '../icons/ArmorIcon'
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    flex: 1,
+    // flex: 1,
     display: 'flex',
     flexDirection: 'column',
     background: colors.blue,
@@ -21,14 +18,14 @@ const styles = theme => ({
   },
   connectHeading: {
     textAlign: 'center',
-    color: colors.white
+    color: colors.white,
   },
   connectContainer: {
-    padding: '20px'
+    padding: '20px',
   },
   actionButton: {
     color: colors.white,
-    borderColor: colors.white
+    borderColor: colors.white,
   },
   notConnectedRoot: {
     display: 'flex',
@@ -41,34 +38,34 @@ const styles = theme => ({
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
-    width: '100%'
+    width: '100%',
   },
   address: {
     color: colors.white,
     width: '100%',
     paddingBottom: '24px',
     display: 'flex',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   balances: {
     color: colors.white,
     width: '100%',
-    padding: '12px'
+    padding: '12px',
   },
   balanceContainer: {
     display: 'flex',
     width: '100%',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   accountHeading: {
-    paddingBottom: '6px'
+    paddingBottom: '6px',
   },
   icon: {
-    cursor: 'pointer'
+    cursor: 'pointer',
   },
   disclaimer: {
     padding: '12px',
-    border: '1px solid '+colors.white,
+    border: '1px solid ' + colors.white,
     borderRadius: '0.75rem',
     marginBottom: '24px',
     fontWeight: 1,
@@ -92,7 +89,7 @@ const styles = theme => ({
     marginRight: '10px',
     '&:visited': {
       color: '#2F80ED',
-    }
+    },
   },
   mainBox: {
     flex: 1,
@@ -101,11 +98,10 @@ const styles = theme => ({
     justifyContent: 'center',
     flexDirection: 'column',
     minHeight: 'calc(100vh - 41px)',
-  }
-});
+  },
+})
 
 class Account extends Component {
-
   constructor(props) {
     super()
 
@@ -116,53 +112,52 @@ class Account extends Component {
   }
 
   render() {
-    const { classes } = this.props;
-    const {
-      modalOpen,
-    } = this.state
+    const { classes } = this.props
+    const { modalOpen } = this.state
 
     return (
-      <div className={ classes.root }>
-        { this.renderNotConnected() }
-        { modalOpen && this.renderModal() }
+      <div className={classes.root}>
+        {this.renderNotConnected()}
+        {modalOpen && this.renderModal()}
       </div>
     )
-  };
+  }
 
   renderNotConnected = () => {
     const { classes } = this.props
     const { loading } = this.state
 
     return (
-      <div className={ classes.notConnectedRoot }>
+      <div className={classes.notConnectedRoot}>
         <div className={classes.mainBox}>
-        <Typography variant={'h5'} className={ classes.disclaimer }>This project is in beta. Use at your own risk.</Typography>
-        <div className={ classes.connectHeading }>
-          <Typography variant='h3'>Connect your wallet to continue</Typography>
-        </div>
-        <div className={ classes.connectContainer }>
-          <Button
-            className={ classes.actionButton }
-            variant="outlined"
-            color="primary"
-            onClick={ this.unlockClicked }
-            disabled={ loading }
+          <Typography variant={'h5'} className={classes.disclaimer}>
+            This project is in beta. Use at your own risk.
+          </Typography>
+          <div className={classes.connectHeading}>
+            <Typography variant="h3">Connect your wallet to continue</Typography>
+          </div>
+          <div className={classes.connectContainer}>
+            <Button
+              className={classes.actionButton}
+              variant="outlined"
+              color="primary"
+              onClick={this.unlockClicked}
+              disabled={loading}
             >
-            <Typography>Connect</Typography>
-          </Button>
-        </div>
+              <Typography>Connect</Typography>
+            </Button>
+          </div>
         </div>
         <a href="https://armor.fi" className={classes.poweredByArmor}>
-          <Typography className={classes.poweredByArmorText}>Powered by Armor</Typography> <ArmorIcon width="25" height="25" />
+          <Typography className={classes.poweredByArmorText}>Powered by Armor</Typography>{' '}
+          <ArmorIcon width="25" height="25" />
         </a>
       </div>
     )
   }
 
   renderModal = () => {
-    return (
-      <UnlockModal closeModal={ this.closeModal } modalOpen={ this.state.modalOpen } />
-    )
+    return <UnlockModal closeModal={this.closeModal} modalOpen={this.state.modalOpen} />
   }
 
   unlockClicked = () => {
@@ -174,4 +169,4 @@ class Account extends Component {
   }
 }
 
-export default withRouter(withStyles(styles)(Account));
+export default withRouter(withStyles(styles)(Account))
